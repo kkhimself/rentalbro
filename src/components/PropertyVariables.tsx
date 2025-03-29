85738bc74a52be2b67e3c9f9ce85d53d1e5094ce
Krishna Kumar <kkhimself@gmail.com>
2025-03-29 13:17:21 -0400

    Organized inputs to sections

--- src/components/PropertyVariables.tsx
+++ src/components/PropertyVariables.tsx
@@ -1,4 +1,4 @@
-import { NumberInput } from "@mantine/core";
+import { Accordion, NumberInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import RentalProperty from "../models/RentalProperty";
 
@@ -61,156 +61,193 @@ export default function PropertyVariables({
 
   return (
     <form>
-      <NumberInput
-        label="Purchase Price"
-        prefix="$"
-        thousandSeparator=","
-        hideControls
-        key={form.key("propertyPurchasePrice")}
-        {...form.getInputProps("propertyPurchasePrice")}
-      />
-      <NumberInput
-        label="Mortgage Down Payment"
-        description={new Intl.NumberFormat("en-US", {
-          style: "currency",
-          currency: "USD",
-        }).format(p.downPayment)}
-        suffix="%"
-        min={0}
-        max={100}
-        step={5}
-        key={form.key("downPaymentPercent")}
-        {...form.getInputProps("downPaymentPercent")}
-      />
-      <NumberInput
-        label="Mortgage Interest Rate"
-        description="Annual interest rate on the mortgage loan."
-        suffix="%"
-        min={0}
-        max={20}
-        step={0.125}
-        key={form.key("mortgageRate")}
-        {...form.getInputProps("mortgageRate")}
-      />
-      <NumberInput
-        label="Mortgage Loan Term (Years)"
-        description="Duration of the mortgage loan. Common terms are 15, 20, or 30 years."
-        min={0}
-        max={50}
-        step={5}
-        key={form.key("mortgageTerm")}
-        {...form.getInputProps("mortgageTerm")}
-      />
-      <NumberInput
-        label="Property Taxes (Yearly)"
-        prefix="$"
-        thousandSeparator=","
-        hideControls
-        key={form.key("propertyTaxesAnnual")}
-        {...form.getInputProps("propertyTaxesAnnual")}
-      />
-      <NumberInput
-        label="HOA Fees (Monthly)"
-        description="Homeowner's Association fees."
-        prefix="$"
-        thousandSeparator=","
-        hideControls
-        key={form.key("hoaFeesMonthly")}
-        {...form.getInputProps("hoaFeesMonthly")}
-      />
-      <NumberInput
-        label="Home Insurance (Yearly)"
-        prefix="$"
-        thousandSeparator=","
-        hideControls
-        key={form.key("homeInsuranceAnnual")}
-        {...form.getInputProps("homeInsuranceAnnual")}
-      />
-      <NumberInput
-        label="Maintenance Costs (Yearly)"
-        description="Cost of repairs, maintenance, and improvements."
-        prefix="$"
-        thousandSeparator=","
-        hideControls
-        key={form.key("maintenanceCostsAnnual")}
-        {...form.getInputProps("maintenanceCostsAnnual")}
-      />
-      <NumberInput
-        label="Rent (Monthly)"
-        prefix="$"
-        thousandSeparator=","
-        hideControls
-        key={form.key("rentMonthly")}
-        {...form.getInputProps("rentMonthly")}
-      />
-      <NumberInput
-        label="Vacancy Rate (Days per Year)"
-        description="Number of days the property may be vacant per year. Average is 10-30 days per year."
-        min={0}
-        max={366}
-        step={10}
-        key={form.key("averageVacancy")}
-        {...form.getInputProps("averageVacancy")}
-      />
-      <NumberInput
-        label="Rent Growth Rate"
-        description="Annual growth rate of rent. Average is 1-3.5% per year in the US."
-        suffix="%"
-        min={0}
-        max={50}
-        step={0.5}
-        key={form.key("rentGrowthRate")}
-        {...form.getInputProps("rentGrowthRate")}
-      />
-      <NumberInput
-        label="Property Appreciation Rate"
-        description="Annual growth rate of property value. Average is 2.5-5% per year in the US."
-        suffix="%"
-        min={0}
-        max={50}
-        step={0.5}
-        key={form.key("propertyValueGrowthRate")}
-        {...form.getInputProps("propertyValueGrowthRate")}
-      />
-      <NumberInput
-        label="Inflation Rate"
-        description="Annual rate of inflation for insurance, maintenance, and other expenses. Average is 2-3% per year in the US."
-        suffix="%"
-        min={0}
-        max={25}
-        step={0.5}
-        key={form.key("inflationRate")}
-        {...form.getInputProps("inflationRate")}
-      />
-      <NumberInput
-        label="Discount Rate"
-        description="Opportunity cost of capital or the expected return from alternative investments, such as average returns of the S&P 500 stock market index."
-        suffix="%"        
-        min={0}
-        max={25}
-        step={0.5}
-        key={form.key("discountRate")}
-        {...form.getInputProps("discountRate")}
-      />
-      <NumberInput
-        label="Holding Period (Years)"
-        description="Duration of time you will own the property before selling it."
-        min={0}
-        max={50}
-        step={5}
-        key={form.key("holdingPeriod")}
-        {...form.getInputProps("holdingPeriod")}
-      />
-      <NumberInput
-        label="Real Estate Agent's Commission"
-        description="Percentage of the property sale price that goes to the real estate agent when selling the property. Average is 5-6% in the US."
-        suffix="%"
-        min={0}
-        max={25}
-        step={0.5}
-        key={form.key("salesCommissionRate")}
-        {...form.getInputProps("salesCommissionRate")}
-      />
+      <Accordion variant="separated" multiple defaultValue={["1", "2", "3", "4", "5"]}>
+        <Accordion.Item value="1">
+          <Accordion.Control>Property Purchase</Accordion.Control>
+          <Accordion.Panel>
+            <NumberInput
+              label="Purchase Price"
+              prefix="$"
+              thousandSeparator=","
+              hideControls
+              key={form.key("propertyPurchasePrice")}
+              {...form.getInputProps("propertyPurchasePrice")}
+            />
+          </Accordion.Panel>
+        </Accordion.Item>
+
+        <Accordion.Item value="2">
+          <Accordion.Control>Financing</Accordion.Control>
+          <Accordion.Panel>
+            <NumberInput
+              label="Mortgage Down Payment"
+              description={new Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD",
+              }).format(p.downPayment)}
+              suffix="%"
+              min={0}
+              max={100}
+              step={5}
+              key={form.key("downPaymentPercent")}
+              {...form.getInputProps("downPaymentPercent")}
+            />
+            <NumberInput
+              label="Mortgage Interest Rate"
+              description="Annual interest rate on the mortgage loan."
+              suffix="%"
+              min={0}
+              max={20}
+              step={0.125}
+              key={form.key("mortgageRate")}
+              {...form.getInputProps("mortgageRate")}
+            />
+            <NumberInput
+              label="Mortgage Loan Term (Years)"
+              description="Duration of the mortgage loan. Common terms are 15, 20, or 30 years."
+              min={0}
+              max={50}
+              step={5}
+              key={form.key("mortgageTerm")}
+              {...form.getInputProps("mortgageTerm")}
+            />            
+          </Accordion.Panel>
+        </Accordion.Item>
+
+        <Accordion.Item value="3">
+          <Accordion.Control>Ongoing Expenses</Accordion.Control>
+          <Accordion.Panel>
+            <NumberInput
+              label="Property Taxes (Yearly)"
+              prefix="$"
+              thousandSeparator=","
+              hideControls
+              key={form.key("propertyTaxesAnnual")}
+              {...form.getInputProps("propertyTaxesAnnual")}
+            />
+            <NumberInput
+              label="HOA Fees (Monthly)"
+              description="Homeowner's Association fees."
+              prefix="$"
+              thousandSeparator=","
+              hideControls
+              key={form.key("hoaFeesMonthly")}
+              {...form.getInputProps("hoaFeesMonthly")}
+            />
+            <NumberInput
+              label="Home Insurance (Yearly)"
+              prefix="$"
+              thousandSeparator=","
+              hideControls
+              key={form.key("homeInsuranceAnnual")}
+              {...form.getInputProps("homeInsuranceAnnual")}
+            />
+            <NumberInput
+              label="Maintenance Costs (Yearly)"
+              description="Cost of repairs, maintenance, and improvements."
+              prefix="$"
+              thousandSeparator=","
+              hideControls
+              key={form.key("maintenanceCostsAnnual")}
+              {...form.getInputProps("maintenanceCostsAnnual")}
+            />            
+          </Accordion.Panel>
+        </Accordion.Item>
+
+        <Accordion.Item value="4">
+          <Accordion.Control>Rental Income</Accordion.Control>
+          <Accordion.Panel>
+            <NumberInput
+              label="Rent (Monthly)"
+              prefix="$"
+              thousandSeparator=","
+              hideControls
+              key={form.key("rentMonthly")}
+              {...form.getInputProps("rentMonthly")}
+            />
+            <NumberInput
+              label="Vacancy Rate (Days per Year)"
+              description="Number of days the property may be vacant per year. Average is 10-30 days per year."
+              min={0}
+              max={366}
+              step={10}
+              key={form.key("averageVacancy")}
+              {...form.getInputProps("averageVacancy")}
+            />
+          </Accordion.Panel>
+        </Accordion.Item>
+
+        <Accordion.Item value="5">
+          <Accordion.Control>Property Sale</Accordion.Control>
+          <Accordion.Panel>
+            <NumberInput
+              label="Holding Period (Years)"
+              description="Duration of time you will own the property before selling it."
+              min={0}
+              max={50}
+              step={5}
+              key={form.key("holdingPeriod")}
+              {...form.getInputProps("holdingPeriod")}
+            />
+            <NumberInput
+              label="Real Estate Agent's Commission"
+              description="Percentage of the property sale price that goes to the real estate agent when selling the property. Average is 5-6% in the US."
+              suffix="%"
+              min={0}
+              max={25}
+              step={0.5}
+              key={form.key("salesCommissionRate")}
+              {...form.getInputProps("salesCommissionRate")}
+            />            
+          </Accordion.Panel>
+        </Accordion.Item>
+
+        <Accordion.Item value="6">
+          <Accordion.Control>Future Growth & Market Trends</Accordion.Control>            
+          <Accordion.Panel>
+            <NumberInput
+              label="Rent Growth Rate"
+              description="Annual growth rate of rent. Average is 1-3.5% per year in the US."
+              suffix="%"
+              min={0}
+              max={50}
+              step={0.5}
+              key={form.key("rentGrowthRate")}
+              {...form.getInputProps("rentGrowthRate")}
+            />
+            <NumberInput
+              label="Property Appreciation Rate"
+              description="Annual growth rate of property value. Average is 2.5-5% per year in the US."
+              suffix="%"
+              min={0}
+              max={50}
+              step={0.5}
+              key={form.key("propertyValueGrowthRate")}
+              {...form.getInputProps("propertyValueGrowthRate")}
+            />
+            <NumberInput
+              label="Inflation Rate"
+              description="Annual rate of inflation for insurance, maintenance, and other expenses. Average is 2-3% per year in the US."
+              suffix="%"
+              min={0}
+              max={25}
+              step={0.5}
+              key={form.key("inflationRate")}
+              {...form.getInputProps("inflationRate")}
+            />
+            <NumberInput
+              label="Discount Rate"
+              description="Opportunity cost of capital or the expected return from alternative investments, such as average returns of the S&P 500 stock market index."
+              suffix="%"        
+              min={0}
+              max={25}
+              step={0.5}
+              key={form.key("discountRate")}
+              {...form.getInputProps("discountRate")}
+            />
+          </Accordion.Panel>
+        </Accordion.Item>
+      </Accordion>
     </form>
   );
 }
